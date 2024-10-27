2c370ea61718a71640b064ea2eff602b7f911a15
emersonzzz <er7682ncr1@stu.nc.k12.mo.us>
2024-10-27 13:52:59 -0500

    Added resources page

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = 'pages/signup.html';
     })
 })
+
+function filterArticles() {
+    console.log('filtering articles')
+    const searchQuery = document.getElementById('search-bar').value.toLowerCase();
+    const articles = document.querySelectorAll('#article-list .article');
+
+    articles.forEach(article => {
+        const articleText = article.textContent.toLowerCase();
+        if (articleText.includes(searchQuery)) {
+            article.style.display = '';
+        } else {
+            article.style.display = 'none';
+        }
+    })
+}
